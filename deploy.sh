7f3bdd6f89c929beda555024ee8468227ce01495
trallarn <trallarn@yahoo.com>
2016-08-19 21:35:12 +0200

    deploys to custom server

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
 # Copies artifacts to dist-folder and copies it to remote server
+# Run as: ./deploy.sh <|user@server>
+# where empty arg deploys to pi
+
+HOST=$1
 
 ./build-dist.sh
 
-scp -r dist/* pi:/var/www/finance/
+# Set destination
+DEST=$(test $HOST && echo $HOST || echo "pi" )
+DEST="$DEST:/var/www/chartmatrix/"
+
+echo "Copying to destination $DEST"
+scp -r dist/* $DEST
